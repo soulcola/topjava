b396f97538497c0ae5de0e9fdb854bb3964435e3
petr <8441404@gmail.com>
2024-07-07 01:04:09 +0300

    HW05 Profiles Main change env

--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -1,6 +1,5 @@
 package ru.javawebinar.topjava;
 
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
@@ -14,28 +13,31 @@ import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
 
+import static ru.javawebinar.topjava.Profiles.REPOSITORY_IMPLEMENTATION;
+import static ru.javawebinar.topjava.Profiles.getActiveDbProfile;
+
 public class SpringMain {
     public static void main(String[] args) {
         // java 7 automatic resource management (ARM)
 //        System.setProperty("spring.profiles.active", Profiles.getActiveDbProfile() + "," + Profiles.REPOSITORY_IMPLEMENTATION);
-        try (ConfigurableApplicationContext appContext = new ClassPathXmlApplicationContext()) {
-            appContext.getEnvironment().setActiveProfiles(Profiles.getActiveDbProfile(), Profiles.REPOSITORY_IMPLEMENTATION);
-            appContext.refresh();
-            try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext(new String[]{"spring/spring-app.xml", "spring/spring-db.xml"}, appContext)) {
-                System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
-                AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
-                adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ADMIN));
-                System.out.println();
 
-                MealRestController mealController = appCtx.getBean(MealRestController.class);
-                List<MealTo> filteredMealsWithExcess =
-                        mealController.getBetween(
-                                LocalDate.of(2020, Month.JANUARY, 30), LocalTime.of(7, 0),
-                                LocalDate.of(2020, Month.JANUARY, 31), LocalTime.of(11, 0));
-                filteredMealsWithExcess.forEach(System.out::println);
-                System.out.println();
-                System.out.println(mealController.getBetween(null, null, null, null));
-            }
+        try (ClassPathXmlApplicationContext appCtx = new ClassPathXmlApplicationContext()) {
+            appCtx.getEnvironment().setActiveProfiles(getActiveDbProfile(), REPOSITORY_IMPLEMENTATION);
+            appCtx.setConfigLocations("spring/spring-app.xml", "spring/spring-db.xml");
+            appCtx.refresh();
+            System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
+            AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
+            adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ADMIN));
+            System.out.println();
+
+            MealRestController mealController = appCtx.getBean(MealRestController.class);
+            List<MealTo> filteredMealsWithExcess =
+                    mealController.getBetween(
+                            LocalDate.of(2020, Month.JANUARY, 30), LocalTime.of(7, 0),
+                            LocalDate.of(2020, Month.JANUARY, 31), LocalTime.of(11, 0));
+            filteredMealsWithExcess.forEach(System.out::println);
+            System.out.println();
+            System.out.println(mealController.getBetween(null, null, null, null));
         }
     }
 }
