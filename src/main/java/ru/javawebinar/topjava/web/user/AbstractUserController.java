4370d8688c6af4488e828889d859fb8fb4fd8d4b
petr <8441404@gmail.com>
2024-08-23 13:23:17 +0300

    11_12_XSS

--- src/main/java/ru/javawebinar/topjava/web/user/AbstractUserController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/AbstractUserController.java
@@ -13,8 +13,8 @@ import ru.javawebinar.topjava.web.validator.UniqueMailValidator;
 
 import java.util.List;
 
-import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
-import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
+import static ru.javawebinar.topjava.util.validation.ValidationUtil.assureIdConsistent;
+import static ru.javawebinar.topjava.util.validation.ValidationUtil.checkNew;
 
 public abstract class AbstractUserController {
     protected final Logger log = LoggerFactory.getLogger(getClass());
